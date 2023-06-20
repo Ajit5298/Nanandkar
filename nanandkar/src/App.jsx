b80df4764858ff6f00bd554f478fdbfd11a0be14
Ajit5298 <vikeekamble01@gmail.com>
2023-06-20 13:29:46 +0530

    done
    pro add using JSON file in men

--- nanandkar/src/App.jsx
+++ nanandkar/src/App.jsx
@@ -9,11 +9,11 @@ import { Registration } from './Components/Ragistration';
 import ConnectUsPage from './Components/ConnectUs';
 import { Navbar } from './Components/Navbar';
 import Chat from './Components/Chat';
-import { Men } from './Components/Men';
 import { Women } from './Components/Women';
 import { Kids } from './Components/Kids';
 import { New } from './Components/New';
 import Sidebar from './Components/Sidebar';
+import { Men } from './Components/Men';
 
 
 
@@ -30,11 +30,11 @@ function App() {
         <Route excat path='conectus' element={<ConnectUsPage />} />
         <Route excat path='nav' element={<Navbar />} />
         <Route excat path='chat' element={<Chat/>} />
-        <Route excat path='men' element={<Men/>} />
         <Route excat path='women' element={<Women/>} />
         <Route excat path='kids' element={<Kids/>} />
         <Route excat path='new' element={<New/>} />
         <Route excat path='sidebar' element={<Sidebar/>} />
+        <Route excat path='men' element={<Men/>} />
      
         
         
